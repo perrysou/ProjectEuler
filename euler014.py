2b0d61a3dada8aa3d8e9d2ed4695b31d3c056e1e
perrysou <ysu27@hawk.iit.edu>
2017-11-09 00:35:42 -0600

    added new problem(s)

--- euler014.py
+++ euler014.py
@@ -16,51 +16,42 @@ NOTE: Once the chain starts the terms are allowed to go above one million.
 """
 
 import random
+nMax = 5 * 10**6
+chainDict = [-1] * (nMax + 1)
+chainDict[1] = 1
+chainMaxDict = [-1] * (nMax + 1)
 
 
-def collatz(n, chainDict):
-    if n not in chainDict:
-        chainDict[n] = 1
-        if n != 1:
-            if n % 2 == 0:
-                n_new = n / 2
-            else:
-                n_new = n * 3 + 1
-            chainDict[n] = collatz(n_new, chainDict) + 1
-    return chainDict[n]
+def collatz(n):
+    n_new = n / 2 if n % 2 == 0 else n * 3 + 1
+    if n <= nMax:
+        if chainDict[n] == -1:
+            chainDict[n] = collatz(n_new) + 1
+        return chainDict[n]
+    else:
+        return collatz(n_new) + 1
 
 
 def main():
+    n_old = 1
+    k_old = 1
+    chainMax = 0
+    ii = 1
     t = int(raw_input().strip())
     # t = random.randint(1, 10 ** 4)
-    chainDict = {}
-    chainMaxDict = {}
-    n_old = 0
-    chainMax = 0
-    k_old = 1
+
     for a in range(t):
         n = long(raw_input().strip())
-        # n = random.randint(1, 5 * 10 ** 5)
-        if n > n_old:
-            step = -1
-        elif n < n_old:
-            step = 1
-            chainMax = 0
-            k_old = 1
-        else:
-            print chainMaxDict[n]
-            continue
-
-        for nn in range(n, max(n_old, n / 2), step):
-            collatz(nn, chainDict)
-        for k in chainDict:
-            if k <= n:
-                if chainDict[k] >= chainMax:
-                    chainMax = chainDict[k]
-                    k_old = max(k, k_old)
-        chainMaxDict[n] = k_old
-        print k_old
-        n_old = n
+        # n = random.randint(1, 5 * 10 ** 6)
+        if chainMaxDict[n] == -1:
+            while ii <= n:
+                if collatz(ii) >= chainMax:
+                    chainMax = chainDict[ii]
+                    k_old = max(ii, k_old)
+                chainMaxDict[ii] = k_old
+                ii += 1
+        print n, chainMaxDict[n]
+        n_old = max(n, n_old)
 
 
 if __name__ == '__main__':

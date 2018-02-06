47f9d2a07b31bce883e80c8cdc3d36bbf9b2a4d4
perrysou <ysu27@hawk.iit.edu>
2018-02-06 16:44:27 -0600

    added new problem(s)

--- euler032.py
+++ euler032.py
@@ -12,16 +12,57 @@ HINT: Some products can be obtained in more than one way so be sure to only incl
 
 import math
 import random
+import itertools
+
+uniques = {}
+
+
+def getPandigital(n):
+    if n == 4:
+        xand = [0]
+        xer = [1]
+        prod = [2]
+    elif n == 5:
+        xand = [0]
+        xer = [1]
+        prod = [3]
+    elif n == 6:
+        xand = [0]
+        xer = [1]
+        prod = [3]
+    elif n == 7:
+        xand = [0, 0]
+        xer = [1, 2]
+        prod = [4, 4]
+    elif n == 8:
+        xand = [0, 0]
+        xer = [1, 2]
+        prod = [4, 4]
+    elif n == 9:
+        xand = [0, 0]
+        xer = [1, 2]
+        prod = [5, 5]
+    sums = 0
+    for i in range(len(xand)):
+        for p in itertools.permutations(range(1, n + 1)):
+            mand = mer = pro = 0
+            for j, d in enumerate(p[xand[i]:xer[i]][::-1]):
+                mand += d * 10**j
+            for j, d in enumerate(p[xer[i]:prod[i]][::-1]):
+                mer += d * 10 ** j
+            for j, d in enumerate(p[prod[i]:][::-1]):
+                pro += d * 10 ** j
+            if mand * mer == pro:
+                if pro not in uniques:
+                    uniques[pro] = n
+                    sums += pro
+    return sums
 
 
 def main():
-    t = int(raw_input().strip())
-    t = random.randint()
-    while t:
-        n = long(raw_input().strip())
-        n = random.randint()
-        t -= 1
-    return 0
+    n = long(raw_input().strip())
+    # n = random.randint(9, 9)
+    print getPandigital(n)
 
 
 if __name__ == '__main__':
